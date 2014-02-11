67207c89509924145018bfa6a8e3776f4e616737
qrrakakh (Hiroaki KURAMAE) <hiroaki.kuramae@gmail.com>
2014-02-11 22:35:08 +0900

    scala-mode configuration

--- .emacs.d/conf/init-scala.el
+++ .emacs.d/conf/init-scala.el
@@ -1,2 +1,19 @@
 (when (require 'scala-mode-auto nil t)
+  (add-to-list 'auto-mode-alist
+               '("\\.scala$" . scala-mode))
+  (defvar my-scala-current-buffer nil)
+
+  (defun my-scala-switch-to-interpreter ()
+    (interactive)
+    (setq my-scala-switch-to-interpreter (current-buffer))
+    (unless (scala-interpreter-running-p-1)
+      (scala-run-scala "scala"))
+    (scala-switch-to-interpreter))
+
+  (defun my-scala-back-to-editing-buffer ()
+    (interactive)
+    (switch-to-buffer my-scala-current-buffer))
+
+  (define-key scala-mode-map "\C-c\C-z" 'my-scala-switch-to-interpreter)
+  (define-key scala-mode-inf-map "\C-c\C-z" 'my-scala-back-to-editing-buffer)  
   )
