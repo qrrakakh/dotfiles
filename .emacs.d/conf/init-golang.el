3c9a3559d87b1dc4d1da87a09032cc801bfaf8e8
qrrakakh (Hiroaki KURAMAE) <hiroaki.kuramae@gmail.com>
2017-09-06 23:02:03 +0900

    Golang Emacs Config mod

--- .emacs.d/conf/init-golang.el
+++ .emacs.d/conf/init-golang.el
@@ -1,1 +1,10 @@
-(when (require 'go-mode-load nil t) )
+(add-to-list 'exec-path (expand-file-name "/home/qrrakakh/local/go/bin"))
+
+
+(when (require 'go-mode nil t)
+  (add-hook 'go-mode-hook (lambda()
+                            (local-set-key (kbd "M-.") 'godef-jump)
+                            (setq indent-tabs-mode nil)    ; タブを利用
+                            (setq c-basic-offset 4)        ; tabサイズを4にする
+                            (setq tab-width 4)))
+    )
